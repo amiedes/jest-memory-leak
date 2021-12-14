7d3c8c2bba52f6ff653171c955ebbc884d3279dd
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-12-14 09:21:28 +0100

    Fix jest imports

--- __tests__/packages/math-service.test.js
+++ __tests__/packages/math-service.test.js
@@ -1,12 +1,8 @@
-// import MathService from '../packages/math-service.js'
+import MathService from '../../packages/math-service.js'
 
 describe('MathService', () => {
   describe('sum', () => {
-    it ('boots', () => {
-      expect(true).toEqual(true)
-    })
-
-    xit('adds two numbers', () => {
+    it('adds two numbers', () => {
       const mathService = new MathService()
       const result = mathService.sum(1, 2)
 
